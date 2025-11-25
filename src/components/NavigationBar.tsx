807292cf45bd5bab61e75aa2e8ceb8d4e82334fa
mehiis <Niko.Mehilainen@Metropolia.fi>
2025-11-25 18:28:26 +0200

    clean navbar code

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -10,19 +10,14 @@ let menu = "";
 
 const NavigationBar = ({ isVisible, mobileMenuOpen, setMobileMenuOpen }: NavigationBarProps) => {
   return (
-            <header className='bg-[var(--primary-color)] min-w-[360px] pb-2 shadow-sm sticky top-0 z-50 transition-transform duration-200 position-absolute px-5' style={{ transform: isVisible ? 'translateY(0)' : 'translateY(-100%)' }}>
-                
+            <header className='bg-[var(--primary-color)] min-w-[360px] shadow-sm sticky top-0 z-50 transition-transform duration-200 position-absolute px-5' style={{ transform: isVisible ? 'translateY(0)' : 'translateY(-100%)' }}>
                 <div className="max-w-[1440px] min-w-[360px] mx-auto flex items-center justify-between bg-[var(--primary-color)] hidden md:flex" >
                     <Link to="/"><img src="/src/assets/images/logos/logo-text-with-desc.png" alt="Logo" className="w-35 h-auto hover:scale-105 transition-transform duration-150"/></Link>
 
-                        <nav className='flex justify-between items-center my-3 bg-[var(--primary-color)'>
-                        {/*<ul className='flex justify-around items-center m-3 p-4'>
-                            <li className='text-[1.5vw] font-[header] mr-5 text-white shadow-md px-5 py-1 bg-[var(--secondary-color)] hover:scale-110 transition-transform duration-300'><button><Link to="/">Home</Link></button></li>
-                            <li className='text-[1.5vw] font-[header] mr-5 text-white shadow-md px-5 py-1 bg-[var(--secondary-color)] hover:scale-110 transition-transform duration-300'><button><Link to="/blogs">Blog</Link></button></li>
-                        </ul>*/} 
+                        <nav className='flex justify-between items-center my-1'>
                         <ul className='flex justify-around items-center m-3 p-4'>
                             <li>
-                                <button className='text-[1.5vw] font-[header] mr-5shadow-md px-5 py-1 hover:scale-115 transition-transform duration-300'>
+                                <button className='text-[1.5vw] font-[header] mr-5 px-4 py-1 hover:scale-115 transition-transform duration-300'>
                                     <a href="/">
                                         <img src="/src/assets/images/buttons/home-button.svg" alt="Home button"/>
                                     </a>
@@ -31,7 +26,7 @@ const NavigationBar = ({ isVisible, mobileMenuOpen, setMobileMenuOpen }: Navigat
 
                             {/* COMING SOON...
                             <li>
-                                <button className='text-[1.5vw] font-[header] mr-5shadow-md px-5 py-1 hover:scale-115 transition-transform duration-300'>
+                                <button className='text-[1.5vw] font-[header] mr-5 px-4 py-1 hover:scale-115 transition-transform duration-300'>
                                 <li>
                                 <button className='text-[1.5vw] font-[header] mr-5shadow-md px-5 py-1 hover:scale-115 transition-transform duration-300'>
                                     <a href="/">
@@ -41,7 +36,7 @@ const NavigationBar = ({ isVisible, mobileMenuOpen, setMobileMenuOpen }: Navigat
                             </li>*/}
 
                             <li>
-                                <button className='text-[1.5vw] font-[header] mr-5shadow-md px-5 py-1 hover:scale-115 transition-transform duration-300'>
+                                <button className='text-[1.5vw] font-[header] mr-5 px-4 py-1 hover:scale-115 transition-transform duration-300'>
                                     <a href="/resume">
                                         <img src="/src/assets/images/buttons/resume-button.svg" alt="Resume button"/>
                                     </a>
@@ -56,10 +51,10 @@ const NavigationBar = ({ isVisible, mobileMenuOpen, setMobileMenuOpen }: Navigat
                     </nav>
                 </div>
 
-                    <div className="flex items-center justify-between bg-[var(--primary-color)] md:hidden" >
+                    <div className="flex items-center justify-between md:hidden" >
                         <Link to="/"><img src="/src/assets/images/logos/logo-text-with-desc.png" alt="Logo" className="w-35 h-auto hover:scale-105 transition-transform duration-150"/></Link>
 
-                        <nav className='flex justify-between items-center my-3 bg-[var(--primary-color)'>
+                        <nav className='flex justify-between items-center my-1'>
                             <div className='flex justify-around items-center'>
                                 <button className='w-[50px] h-[50px]' onClick={() => setMobileMenuOpen(!mobileMenuOpen)} style={{ display: mobileMenuOpen ? menu="/src/assets/images/misc/close-menu.svg" : menu="/src/assets/images/misc/mobile-menu.svg"}}>
                                     <img src={menu} alt="Mobile Menu button"/>
