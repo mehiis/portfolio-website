18b6e54d28c9321b2f417c9a9a267b22aa18524c
mehiis <Niko.Mehilainen@Metropolia.fi>
2025-12-02 15:06:48 +0200

    update 404 page and polish me component

--- src/components/Me.tsx
+++ src/components/Me.tsx
@@ -7,16 +7,15 @@ const Me = () => {
       <section className='max-w-[1440px] min-w-[360px] mx-auto p-2'>
 
           <div className='p-4 m-4'>
-            <div className="bg-white w-full rounded-lg flex flex-wrap gap-4 lg:px-15 p-8 md:p-15 lg:p-15">
+            <div className="bg-[var(--white-color)] w-full rounded-lg flex flex-wrap gap-4 lg:px-15 p-8 md:p-15 lg:p-15">
                 <h3 className="font-[arial-black] lg:text-3xl md:text-3xl text-2xl">Hi, I'm Niko</h3>
                 <p className="lg:text-3xl md:text-3xl text-lg font-[arial]">
                     <br/>
-                    I'm a {month >= 0 ?  age  : age - 1}-year-old software developer from Helsinki, Finland and I thank you for showing interest in my portfolio!
+                    I'm a {month >= 0 ?  age  : age - 1}-year-old software developer from Helsinki, Finland and I thank you for showing interest in my portfolio! &lt;3 
+  
+                    <br/><br/>I started out building small, quirky games in Unity, and that creative spark never really went away. I love designing and making games, and generally anything that’s visually polished, functional, and fun to use. I’m the kind of person who gets unreasonably excited about clean UI and small technical victories. I'm always looking for new ways to improve my knowledge and skills, and I thrive in environments that challenge me to think from a new perspective.
 
-                    <b> &lt;3</b> <br/><br/>I’ve always enjoyed playing around with code, so ending up pursuing this field both academically and professionally feels pretty natural. My programming journey kicked off around 2014, when I discovered the Unity game engine and ever since, I’ve been coding on and off.
-
-                    <br/><br/>
-                    I started out building small, quirky games in Unity, and that creative spark never really went away. I still love designing and making games, and generally anything that’s visually polished, functional, and fun to use. I’m the kind of person who gets unreasonably excited about clean UI and small technical victories.
+                    <br/><br/>I'm always open to chat, so feel free to reach out if you'd like to discuss about potential collaborations or just want to say hi! You can find my contact information from my resume or reach out via social media. Let's create something amazing together!
                 </p>
               </div>
           </div>
--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,13 +1,16 @@
+import header from '@/assets/images/misc/fouronefour.png'
+import notFound from '@/assets/images/misc/not-found.png'
+import goBack from '@/assets/images/buttons/go-back.png'
+
 const NotFound = () => {
   return (
-    <div className='mx-auto py-50 center'>
-        <div className='max-w-[1600px] mx-auto text-white p-5'>
-            <h2 className="text-[4vw] font-[header] text-black italic text-center">OOPS.. Page Not Found! :(</h2>
-            <p className="text-[1.3vw] text-gray-700 p-10 bg-[var(--white-color)] m-4 text-center">
-                The page you are looking for does not exist.
-            </p>
-        </div>
-    </div>
+    <section className='mx-auto flex flex-col items-center justify-center min-h-screen'>
+            <img src={header} alt="404 Not Found" className='mb-10 h-auto w-[20vh]'/>
+            <img src={notFound} alt="Page you were looking for does not exist." className='mb-20 h-auto w-[40vh]'/>
+            <button onClick={() => window.history.back()} >
+                <img src={goBack} alt="Go Back Button" className='hover:scale-110 transition-transform h-auto w-[25vh]'/>
+            </button>
+    </section>
   )
 }
 
