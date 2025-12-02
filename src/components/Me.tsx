90e477ad3cf4039c9d00bae1fe0f8d1d827515ee
mehiis <Niko.Mehilainen@Metropolia.fi>
2025-12-02 15:12:50 +0200

    polish me component

--- src/components/Me.tsx
+++ src/components/Me.tsx
@@ -13,7 +13,8 @@ const Me = () => {
                     <br/>
                     I'm a {month >= 0 ?  age  : age - 1}-year-old software developer from Helsinki, Finland and I thank you for showing interest in my portfolio! &lt;3 
   
-                    <br/><br/>I started out building small, quirky games in Unity, and that creative spark never really went away. I love designing and making games, and generally anything that’s visually polished, functional, and fun to use. I’m the kind of person who gets unreasonably excited about clean UI and small technical victories. I'm always looking for new ways to improve my knowledge and skills, and I thrive in environments that challenge me to think from a new perspective.
+                    <br/><br/>I started out building small, quirky games in Unity, and that creative spark never really went away. I love designing and making games, and generally anything that’s visually polished, functional, and fun to use. 
+                    <br/><br/>I’m the kind of person who gets unreasonably excited about clean UI and small technical victories. I'm always looking for new ways to improve my knowledge and skills, and I thrive in environments that challenge me to think from a new perspective.
 
                     <br/><br/>I'm always open to chat, so feel free to reach out if you'd like to discuss about potential collaborations or just want to say hi! You can find my contact information from my resume or reach out via social media. Let's create something amazing together!
                 </p>
