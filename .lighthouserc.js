ca3ca5d842bac2c1190b6de6d2849b574a7fc5d8
beeboy98 <niko.mehilainen@gmail.com>
2026-01-29 14:31:24 +0200

    actions test

---
+++ .lighthouserc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  ci: {
+    collect: {
+      // Jos sinulla on staattinen sivusto (esim. build-kansio)
+      staticDistDir: './dist',
+      // Tai jos haluat testata livenä olevaa URL-osoitetta:
+      // url: ['https://sinunportfoliosi.fi'],
+    },
+    assert: {
+      assertions: {
+        // Tässä asetetaan kriteerit. 'error' pysäyttää CI-putken.
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage', // Tallentaa raportin väliaikaisesti pilveen katsottavaksi
+    },
+  },
+};
