e0a2aa57ead21142d69c585be79223e4fa6af342
beeboy98 <niko.mehilainen@gmail.com>
2026-01-29 14:52:27 +0200

    remove 2nd inbug

--- src/components/general/Footer.tsx
+++ src/components/general/Footer.tsx
@@ -1,5 +1,5 @@
 import github from "@/assets/images/icons/github-mark.svg";
-import linkedin from "@/assets/images/icons/inBug-Black.png";
+{/*import linkedin from "@/assets/images/icons/inBug-Black.png";*/}
 import instagram from "@/assets/images/icons/ig.png";
 import fullNameLogo from "@/assets/images/logos/full-name.png"
 import { Link } from "react-router";
@@ -16,7 +16,7 @@ const Footer = () => {
 
                 <div className='flex justify-center'>
                     <a href="https://github.com/mehiis" target="_blank"><img src={github} alt="GitHub Logo 2025" className="w-8 h-auto mx-3 hover:scale-115 transition-transform duration-150" /></a>
-                    <a href="https://fi.linkedin.com/in/nikomehilainen" target="_blank"><img src={linkedin} alt="LinkedIn Logo 2025" className="w-8 h-auto mx-3 hover:scale-115 transition-transform duration-150" /></a>
+                    {/*<a href="https://fi.linkedin.com/in/nikomehilainen" target="_blank"><img src={linkedin} alt="LinkedIn Logo 2025" className="w-8 h-auto mx-3 hover:scale-115 transition-transform duration-150" /></a>*/}
                     <a href="https://www.instagram.com/nikomehilainen/" target="_blank"><img src={instagram} alt="Instagram Logo 2025" className="w-8 h-auto mx-3 hover:scale-115 transition-transform duration-150" /></a>
                 </div>
             </div>
