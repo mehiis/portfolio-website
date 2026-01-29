4fe4df5df2dd419d77de4d1b073f4a7dc8a47118
beeboy98 <niko.mehilainen@gmail.com>
2026-01-29 22:13:34 +0200

    fix lcp

--- src/components/homepage/Education.tsx
+++ src/components/homepage/Education.tsx
@@ -18,7 +18,6 @@ const Education = () => {
   return (
         <div className="max-w-[1440px] min-w-[360px] mx-auto p-2 md:px-30">
             <CustomH2Title source={titleIcon} alt="Education" />
-
             <div className='p-4 m-4'>
 
                 <ul className="bg-[var(--primary-color)] w-full rounded-lg grid grid-cols-1 md:grid-cols-2 gap-4 px-4 py-10 shadow-sm">
--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -3,23 +3,23 @@ import heroImg from "@/assets/images/misc/hero-image.png";
 function Hero() {
     return (
           <>
-          <section className="py-10 items-center bg-[var(--primary-color)] px-0 shadow-sm 2xl:py-25">
-              <div
-              className="max-w-[1440px] min-w-[360px] m-auto md:flex md:justify-between sm:center items-center justify-center md:px-10 lg:px-35">
-                  <img
-                  className="object-contain w-2/3 sm:w-1/2 md:w-1/4 mx-auto md:mx-0 mb-10 md:mb-0"
-                  alt="Hero Image 2025"
-                  fetchPriority="high"
-                  loading="eager"
-                  src={heroImg}
-                  />
+            <section className="py-10 items-center bg-[var(--primary-color)] px-0 shadow-sm 2xl:py-25">
+                <div
+                className="max-w-[1440px] min-w-[360px] m-auto md:flex md:justify-between sm:center items-center justify-center md:px-10 lg:px-35">
+                    <img
+                      className="object-contain w-2/3 sm:w-1/2 md:w-1/4 mx-auto md:mx-0 mb-10 md:mb-0"
+                      alt="Hero Image 2025"
+                      fetchPriority="high"
+                      loading="eager"
+                      src={heroImg}
+                    />
 
-                  <p className="text-xl font-[LeagueSpartanRegular] bg-[var(--white-color)] text-[var(--black-color)] shadow-sm rounded-lg my-auto  mx-10 p-8">
-                    What I love most about programming is its <b>creative power</b> to turn ideas into immersive, meaningful experiences. <br></br> <br></br>It brings together various forms of art and has the <b>unique ability to connect people</b>.
-                  </p>
-              </div>
-          </section>
-</>
+                    <p className="text-xl font-[LeagueSpartanRegular] bg-[var(--white-color)] text-[var(--black-color)] shadow-sm rounded-lg my-auto  mx-10 p-8">
+                      What I love most about programming is its <b>creative power</b> to turn ideas into immersive, meaningful experiences. <br></br> <br></br>It brings together various forms of art and has the <b>unique ability to connect people</b>.
+                    </p>
+                </div>
+            </section>
+          </>
         );
 }
 
