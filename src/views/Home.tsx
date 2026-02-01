dc29b4d5228c73360eccec44d87c64f59c326d71
mehiis <Niko.Mehilainen@Metropolia.fi>
2026-02-01 20:25:10 +0200

    add work + tools

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,12 +1,16 @@
 import Education from '../components/homepage/Education'
 import Hero  from '../components/homepage/Hero'
 import Me from '../components/homepage/Me'
+import Tools from '../components/homepage/Tools'
+import Work from '../components/homepage/Work'
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Work />
       <Education />
+      <Tools />
       <Me />
     </>
   )
