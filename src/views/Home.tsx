ea447f79559fcddad25c98fb0f395633bc6e9793
mehiis <Niko.Mehilainen@Metropolia.fi>
2026-02-01 20:21:52 +0200

    add ed + me

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,13 @@
+import Education from '../components/homepage/Education'
 import Hero  from '../components/homepage/Hero'
+import Me from '../components/homepage/Me'
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Education />
+      <Me />
     </>
   )
 }
