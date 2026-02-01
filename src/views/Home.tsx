631510168ba6110528b6d109d1d0324e96a6c393
mehiis <Niko.Mehilainen@Metropolia.fi>
2026-02-01 20:17:14 +0200

    remove components from homepage

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,19 +1,9 @@
 import Hero  from '../components/homepage/Hero'
-import Tools from '../components/homepage/Tools'
-import Personal from '../components/homepage/Personal'
-import Me from '../components/homepage/Me'
-import Education from '../components/homepage/Education'
-import Work from '../components/homepage/Work'
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <Personal />
-      <Work />
-      <Education />
-      <Tools />
-      <Me />
     </>
   )
 }
