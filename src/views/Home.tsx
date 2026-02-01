b310312b095fbd2439fc5e39efbbde94b0b5fb7f
mehiis <Niko.Mehilainen@Metropolia.fi>
2026-02-01 20:29:04 +0200

    add personal project component

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,7 @@
 import Education from '../components/homepage/Education'
 import Hero  from '../components/homepage/Hero'
 import Me from '../components/homepage/Me'
+import Personal from '../components/homepage/Personal'
 import Tools from '../components/homepage/Tools'
 import Work from '../components/homepage/Work'
 
@@ -8,6 +9,7 @@ const Home = () => {
   return (
     <>
       <Hero />
+      <Personal />
       <Work />
       <Education />
       <Tools />
