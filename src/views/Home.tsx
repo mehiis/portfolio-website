497f3709fd125648d409fe44c6db8d61d9eba5d6
mehiis <Niko.Mehilainen@Metropolia.fi>
2026-02-02 11:29:29 +0200

    remove work

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,13 +2,11 @@ import Education from '../components/homepage/Education'
 import Hero  from '../components/homepage/Hero'
 import Me from '../components/homepage/Me'
 import Tools from '../components/homepage/Tools'
-import Work from '../components/homepage/Work'
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <Work />
       <Education />
       <Tools />
       <Me />
