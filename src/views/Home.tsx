d6487cb321afd402b8f0ca4c0e81d60c574ef4a6
mehiis <Niko.Mehilainen@Metropolia.fi>
2026-02-02 11:33:16 +0200

    return personal

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,12 +1,14 @@
 import Education from '../components/homepage/Education'
 import Hero  from '../components/homepage/Hero'
 import Me from '../components/homepage/Me'
+import Personal from '../components/homepage/Personal'
 import Tools from '../components/homepage/Tools'
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Personal />
       <Education />
       <Tools />
       <Me />
