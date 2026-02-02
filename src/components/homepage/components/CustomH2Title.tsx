8ff5aad5d7a86f34302ea15fe6cab6fca723ae44
mehiis <Niko.Mehilainen@Metropolia.fi>
2026-02-02 11:18:33 +0200

    return empty h2

--- src/components/homepage/components/CustomH2Title.tsx
+++ src/components/homepage/components/CustomH2Title.tsx
@@ -6,7 +6,14 @@
 const CustomH2Title = (props: Props) => {
     return (
     <h2>
-
+        <img
+        src="#"
+        alt={"A topic title with text " + props.alt + "."}
+        className="px-7 pt-10 w-auto h-[70px]"
+        width="400"
+        height="40"
+        loading="lazy"
+        />
         
     </h2>
   )
